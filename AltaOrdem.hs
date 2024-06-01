77b97296501f64b403d56408194090243cfa627c
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-06-01 15:31:17 -0300

    Lista alta ordem haskell

---
+++ AltaOrdem.hs
@@ -0,0 +1,74 @@
+-- Questao 1
+-- quicksort
+quicksort :: [Int] -> [Int]
+quicksort [] = []
+quicksort (x:xs) = quicksort menores ++ [x] ++ quicksort maiores
+    where
+        menores = [a | a <- xs, a < x]
+        maiores = [a | a <- xs, a >= x]
+
+impares :: [Int] -> [Int]
+impares (x:xs) = filter odd $ quicksort (x:xs)
+
+-- Questao 2
+posicao :: Int -> [a] -> a
+posicao pos xs = xs !! pos
+
+-- Questao 3 
+repete :: Int -> [[Int]]
+repete valor | valor == 0 = []
+             | otherwise = [take valor (repeat valor)] ++ repete (valor - 1)
+
+-- Questao 4
+palindromo :: Eq a => [a] -> Bool
+palindromo xs | xs == reverse xs = True
+              | otherwise = False
+
+-- Questao 5
+fibonacci :: Int -> [Int]
+fibonacci 0 = []
+fibonacci 1 = [0]
+fibonacci 2 = [0, 1]
+fibonacci n = 0 : 1 : zipWith (+) (tail (fibonacci (n-1))) (fibonacci (n-1))
+
+-- Questao 6
+--a)
+all' :: (a -> Bool) -> [a] -> Bool
+all' _ [] = True
+all' p (x:xs) | not (p x) = False
+              | otherwise = all' p xs
+
+--b)
+any' :: (a -> Bool) -> [a] -> Bool
+any' _ [] = False
+any' p (x:xs) | p x = True
+              | otherwise = any' p xs
+
+--c)
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' p (x:xs) | p x = x : takeWhile' p xs
+                    | otherwise = takeWhile' p xs
+
+--d)
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' _ [] = []
+dropWhile' p (x:xs) | p x = dropWhile' p xs
+                    | otherwise = x : dropWhile' p xs
+
+-- Questao 7
+map' f = foldr (\x xs -> f x : xs) []
+filter' f = foldr (\x xs -> if f x then x : xs else xs) []
+
+-- Questao 8
+dec2int :: [Int] -> Int
+dec2int = foldl (\x y -> 10 * x + y) 0
+
+-- Questao 9
+unfold p h t x 
+       | p x = []
+       | otherwise = h x : unfold p h t (t x)
+
+int2bin = unfold (== 0) (`mod` 2) (`div` 2)
+
+map'' p (x:xs) = unfold p x 
