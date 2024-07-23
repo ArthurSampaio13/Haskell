cca6bf499e6fe1942299b0ca9f669267c0fd5b38
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-07-23 17:46:45 -0300

    feat: lista classes

--- Classes/listaClasses.hs
+++ Classes/listaClasses.hs
@@ -1,6 +1,6 @@
 data Nat = Zero | Suc Nat deriving Show
 
--- Questa 1
+-- Questao 1
 nat2int :: Nat -> Int
 nat2int Zero = 0
 nat2int (Suc n) = 1 + nat2int n
@@ -12,27 +12,51 @@ int2nat n = Suc (int2nat (n-1))
 somar :: Nat -> Nat -> Nat
 somar m n = int2nat (nat2int m + nat2int n)
 
-mult :: Nat -> Nat -> Nat
-mult Zero _ = Zero
-mult _ Zero = Zero
-mult m (Suc n) = somar m (mult m n)
+somar2 :: Nat -> Nat -> Nat
+somar2 Zero n = n
+somar2 (Suc m) n = Suc (somar m n)
+
+multi :: Nat -> Nat -> Nat
+multi _ Zero = Zero
+multi Zero _ = Zero
+multi (Suc m) n = somar (multi m n) n
 
 -- Questao 2
-data Arvore a = Folha a | No (Arvore a) a (Arvore a)
+data ArvoreBinaria a = Folha1 a | No1 (ArvoreBinaria a) a (ArvoreBinaria a) deriving Show
 
-existe :: Ord a => a -> Arvore a -> Bool
-existe x (Folha a) = x == a
-existe x (No esq meio dir) = (x == meio) || existe x esq || existe x dir
+existe :: Ord a => a -> ArvoreBinaria a -> Bool
+existe v (Folha1 f) = f == v
+existe v (No1 esq meio dir) = v == meio || existe v esq || existe v dir
 
 -- Questao 3
-conta :: Arvore a -> Int
-conta (Folha a) = 1
-conta (No esq meio dir) = 0 + conta esq + conta dir
+data Arvore a = Folha a | No (Arvore a) (Arvore a) deriving Show
+contaFolhas :: Arvore a -> Int
+contaFolhas (Folha a) = 1
+contaFolhas (No esq dir) = contaFolhas esq + contaFolhas dir
 
 balanceada :: Arvore a -> Bool
-balanceada (Folha a) = False
-balanceada (No esq meio dir) | conta esq == conta dir = True
-                             | otherwise = False
+balanceada (Folha a) = True
+balanceada (No esq dir) = contaFolhas esq == contaFolhas dir
 
 -- Questao 4
+dividir :: [a] -> ([a], [a])
+dividir lista = splitAt (length lista `div` 2) lista
+
+balancear :: [a] -> Arvore a 
+balancear [a] = Folha a
+balancear lista = No (balancear esq) (balancear dir)
+                    where
+                        (esq, dir) = dividir lista
+
+-- Questao 5
+data Expr = Val Int | Add Expr Expr
+
+avaliar :: Expr -> Int
+avaliar (Val a) = a
+avaliar (Add x y) = avaliar x + avaliar y
+
+-- Questao 6
+
+
+
 
