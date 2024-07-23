c08fd7ef1293d779015a80b50c064ea28098d078
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-07-23 19:54:25 -0300

    revisao lista classes

--- Classes/revisaoClasses.hs
+++ Classes/revisaoClasses.hs
@@ -11,7 +11,7 @@ int2nat n = Suc (int2nat (n-1))
 
 somar :: Nat -> Nat -> Nat
 somar m n = int2nat (nat2int m + nat2int n)
-s
+
 somar2 :: Nat -> Nat -> Nat
 somar2 Zero n = n
 somar2 (Suc m) n = Suc (somar m n)
@@ -22,11 +22,11 @@ multi Zero _ = Zero
 multi (Suc x) y = somar (multi x y) y
 
 -- Questao 2
-data ArvoreBinaria a = Folha a | No (ArvoreBinaria a) a (ArvoreBinaria a) deriving Show
+data ArvoreBinaria a = Folha1 a | No1 (ArvoreBinaria a) a (ArvoreBinaria a) deriving Show
 
 existe :: Ord a => a -> ArvoreBinaria a -> Bool 
-existe x (Folha y) = x == y
-existe x (No esq meio dir) = x == meio || existe esq || existe dir
+existe x (Folha1 y) = x == y
+existe x (No1 esq meio dir) = x == meio || existe x esq || existe x dir
 
 -- Questao 3
 data Arvore a = Folha a | No (Arvore a) (Arvore a)
@@ -41,13 +41,13 @@ balanceada (No esq dir) = contaFolhas esq == contaFolhas dir
 
 -- Questao 4
 divideLista :: [a] -> ([a], [a])
-divideLista lista = splitAt (lenght lista `div` 2) lista
+divideLista lista = splitAt (length lista `div` 2) lista
 
 balancear :: [a] -> Arvore a
 balancear [a] = Folha a
 balancear lista = No (balancear esq) (balancear dir)
                     where
-                        (esq, dir) = splitAt lista
+                        (esq, dir) = divideLista lista
 
 -- Questao 5
 data Expr = Val Int | Add Expr Expr
@@ -57,9 +57,9 @@ avaliar (Val a) = a
 avaliar (Add x y) = avaliar x + avaliar y
 
 -- Questao 6
-data Expr = Val2 Int | Op2 Expr Expr
+data Expr2 = Val2 Int | Op2 Expr2 Expr2
 
-folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
+folde :: (Int -> a) -> (a -> a -> a) -> Expr2 -> a
 folde f _ (Val2 i) = f i
 folde f g (Op2 e1 e2) = g (folde f g e1) (folde f g e2)
 
